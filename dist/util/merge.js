d33c6a32285812262da4571ec587caaa2c9f40da
Wyatt Henke <pcdriverpro@gmail.com>
2022-12-08 16:51:58 +0000

    cleanup

--- dist/util/merge.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.merge = void 0;
-const merge = (...varArgs) => {
-    return Object.assign({}, ...varArgs);
-};
-exports.merge = merge;
-//# sourceMappingURL=merge.js.map
